90b335a676018623f946397417b4f38b6182ecf9
Rafael López Dorado <rldorado86@gmail.com>
2024-12-17 16:50:39 +0100

    Project Structure and Routing

--- eslint.config.js
+++ eslint.config.js
@@ -12,8 +12,12 @@ export default [
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
   ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    }
+  },
   ...vueTsEslintConfig(),
   skipFormatting,
 ]
